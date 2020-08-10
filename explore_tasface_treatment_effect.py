2e6f8f2fe360c9cbe109f6c51967e40a512cf56f
mdekauwe <mdekauwe@gmail.com>
2020-08-10 22:40:24 +1000

    Added nicer plot

--- explore_tasface_treatment_effect.py
+++ explore_tasface_treatment_effect.py
@@ -71,7 +71,7 @@ def main(met, lai, soil_volume, theta_sat):
 
     return (out)
 
-def calc_beta(theta, theta_fc=0.4, theta_wp=0.1):
+def calc_beta(theta, theta_fc=0.35, theta_wp=0.1):
 
     beta = (theta - theta_wp) / (theta_fc - theta_wp)
     beta = max(0.0, beta)
@@ -108,7 +108,13 @@ def store_hourly(idx, An, et, lai_leaf, precip, store, hours_in_day):
     store.An_can[idx] = np.sum(An * an_conv)
     store.E_can[idx] = np.sum(et * et_conv)
     store.LAI_can[idx] = np.sum(lai_leaf)
-    store.delta_sw[idx] = (precip * precip_conv) - store.E_can[idx]
+
+    precip *= precip_conv
+    precip_max = 1.0    # hack for runoff
+    if precip > precip_max:
+        precip = precip_max
+
+    store.delta_sw[idx] = precip - store.E_can[idx]
 
 def store_daily(year, doy, idx, store, soil_volume, theta_sat, beta, out):
 
@@ -123,6 +129,13 @@ def store_daily(year, doy, idx, store, soil_volume, theta_sat, beta, out):
         prev_sw = theta_sat
     else:
         prev_sw = out.sw[idx-1]
+
+    esoil = 3.0 # mm day-1
+    delta = np.sum(store.delta_sw) - esoil
+    delta_max = 3.0  # hacky, fix
+    if delta > delta_max:
+        delta = delta_max
+
     out.sw[idx] = update_sw_bucket(np.sum(store.delta_sw), prev_sw,
                                    soil_volume, theta_sat)
 
@@ -203,18 +216,84 @@ if __name__ == "__main__":
     met = met[ ((met.index.year == 2003) & (met.doy >= 260)) |
                ((met.index.year == 2004) & (met.doy <= 90)) ]
 
+    time = met.copy()
+    time_day = time.resample('D').mean()
+    time_day = time_day.index
 
-    # Need to create an LAI harvest timeseries, for now fix it.
-    lai = np.ones(len(met)) * 1.0
+    aCa = met.ca.values.copy()
+    eCa = aCa * 1.6
+    #print(np.mean(aCa), np.mean(eCa))
+
+    #print( np.sum(met.precip * 3600.0) )
+    #sys.exit()
+
+    # Need to create an LAI harvest timeseries
+    lai = np.zeros(len(met))
+    lai_max = 1.0
+    days_to_max = 13
+    cnt = 0
+    for i in range(len(lai)):
+
+        if cnt == 0.0:
+            laix = 0.0
+        else:
+            laix += lai_max / float(days_to_max)
+
+        lai[i] = laix
+
+        #print(i, cnt, lai[i])
+
+        cnt += 1
+        if cnt > days_to_max:
+            cnt = 0
+
+    #lai = np.ones(len(met)) * 1.0
 
 
     soil_depth = 0.2 # depth of soil bucket, m
     ground_area = 1.0 # m
     soil_volume = ground_area * soil_depth # m3
-    theta_sat = 0.5
-    out = main(met, lai, soil_volume, theta_sat)
+    theta_sat = 0.4
+    out_aCa = main(met, lai, soil_volume, theta_sat)
 
-    #plt.plot(out.An_can)
-    #plt.plot(out.sw, out.beta, "ko")
-    plt.plot(out.sw)
-    plt.show()
+    met.ca *= 1.6
+    out_eCa = main(met, lai, soil_volume, theta_sat)
+
+
+    fig = plt.figure(figsize=(18,6))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.2)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 12
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+
+
+
+    ax1.plot(time_day, out_aCa.sw, "b-", label="Amb")
+    ax1.plot(time_day, out_eCa.sw, "r-", label="Ele")
+    ax1.legend(numpoints=1, loc="best", frameon=False)
+
+    ax1.set_ylabel("SWC (m$^{3}$ m$^{-3}$)")
+
+    ax2.plot(out_aCa.sw, out_aCa.beta, "bo")
+    ax2.plot(out_eCa.sw, out_eCa.beta, "ro")
+
+    ax2.set_xlabel("SWC (m$^{3}$ m$^{-3}$)")
+    ax2.set_xlabel("Beta (-)")
+
+    ax3.plot(time_day, out_aCa.An_can, "b-")
+    ax3.plot(time_day, out_eCa.An_can, "r-")
+
+    ax3.set_ylabel("An (g C m$^{-2}$ d$^{-1}$)")
+
+    fig.autofmt_xdate()
+    fig.savefig("blah.png", bbox_inches='tight', pad_inches=0.1)
